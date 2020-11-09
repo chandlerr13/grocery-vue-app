2a3c1c27d16f7042a582b855c2f554ad6f1fc2c8
Chandler Rogers <chandlerrogers@Chandlers-MacBook-Pro.local>
2020-11-08 19:29:10 -0700

    vue cli setup, all code for views and components

---
+++ src/views/Cart.vue
@@ -0,0 +1,51 @@
+<template>
+<div>
+  <div class="wrapper">
+    <h1>Your Cart</h1>
+    <img src="../assets/shopping_cart.png" id="cartImg" >
+  </div>
+  <div v-if="cartEmpty" class="wrapper">
+    <p id="empty">~ Cart is empty ~</p>
+  </div>
+  <div v-else>
+    <CartList :cartItems="getItems" />
+  </div>
+</div>
+</template>
+
+<script>
+import CartList from "../components/CartList.vue";
+export default {
+  name: 'Cart',
+  components: {
+    CartList
+  },
+  computed: {
+    getItems() {
+      return this.$root.$data.cart;
+    },
+    cartEmpty() {
+      return this.$root.$data.cart.length == 0;
+    }
+  },
+}
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+#empty {
+  font-style: italic;
+}
+
+#cartImg {
+  width: 100px;
+  margin: 25px;
+}
+
+</style>
